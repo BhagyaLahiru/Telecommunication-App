adcd61782a831c872a1a19872e6ade337b819741
BhagyaLahiru <blkgamage@gmail.com>
2021-10-25 14:05:11 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> getAllCustomers();
+
+    Customer saveCustomer(Customer customer);
+
+    Customer getCustomerById(Long id);
+
+    Customer updateCustomer(Customer customer);
+
+    void deleteCustomerById(Long id);
+}
